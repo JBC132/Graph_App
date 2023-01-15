d85faac3d445acd1a4999fac3c3193a88a6a1697
이진영 <jakerhee321@gmail.com>
2023-01-15 13:35:00 +0900

    Input Button

--- Graph_App.py
+++ Graph_App.py
@@ -1,14 +1,15 @@
 import PySimpleGUI as sg
 
+sg.theme('DarkTea18')
 table_content = []
-
 layout = [
     [sg.Table(
         headings = ['Observation', 'Result'], 
         values = table_content, 
         expand_x=True, 
         hide_vertical_scroll=True,
-        key = '-TABLE-')]
+        key = '-TABLE-')],
+    [sg.Input(key = '-INPUT-', expand_x = True), sg.Button('Submit')]
 ]
 
 window = sg.Window('Graph App', layout)
