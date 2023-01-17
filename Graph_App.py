3bad8b816c00d11dad7684f287628447bf1a53a3
이진영 <jakerhee321@gmail.com>
2023-01-17 22:51:43 +0900

    Import matplotlib

--- Graph_App.py
+++ Graph_App.py
@@ -1,4 +1,6 @@
 import PySimpleGUI as sg
+import matplotlib
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 sg.theme('DarkTea16')
 table_content = []
@@ -9,10 +11,13 @@ layout = [
         expand_x=True, 
         hide_vertical_scroll=True,
         key = '-TABLE-')],
-    [sg.Input(key = '-INPUT-', expand_x = True), sg.Button('Submit')]
+    [sg.Input(key = '-INPUT-', expand_x = True), sg.Button('Submit')],
+    [sg.Canvas(key = '-CANVAS-')]
 ]
 
 window = sg.Window('Graph App', layout)
+
+fig = matplotlib.figure.Figure(figsize = (5,4))
 
 while True:
     event, values = window.read()
@@ -24,5 +29,6 @@ while True:
         if new_value.isnumeric():
             table_content.append([len(table_content) + 1,float(new_value)])
             window['-TABLE-'].update(table_content)
+            window['-INPUT-'].update('')
 
 window.close()
